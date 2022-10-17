76c728eee431485b773e065428964cb3839d9505
hasindu sithmin <hasindusithmin64@gmail.com>
2022-10-17 06:32:56 +0000

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,18 +6,16 @@ export default function Home() {
         <div className="w3-main" style={{ marginLeft: '250px' }}>
             <div className="w3-hide-large" style={{ marginTop: '83px' }}></div>
             <header className="w3-container w3-xlarge">
-                <p className="w3-left">Jeans</p>
-                <p className="w3-right">
-                <i className="fa fa-shopping-cart w3-margin-right"></i>
-                <i className="fa fa-search"></i>
+                <p className="w3-left w3-text-grey"><b>TRUE NEWS</b></p>
+                <p className="w3-right w3-text-grey">
+                    <i className="fa fa-search"></i>
                 </p>
             </header>
             <div className="w3-display-container w3-container">
-            <img src={news} alt="News" style={{width:'100%'}} />
-            <div className="w3-display-topleft w3-text-white" style={{padding:'24px 48px'}}>
-            <h1 className="w3-jumbo w3-hide-small">New arrivals</h1>
-            <h1 className="w3-hide-large w3-hide-medium">New arrivals</h1>
-            <h1 className="w3-hide-small">COLLECTION 2016</h1>
+                <img src={news} alt="News" style={{width:'100%'}} />
+                <div className="w3-display-topleft w3-text-white" style={{padding:'24px 48px'}}>
+                <h1 className="w3-jumbo w3-hide-small w3-cursive">We Report You Decide</h1>
+                <h1 className="w3-hide-large w3-hide-medium w3-cursive">We Report You Decide</h1>
             </div>
         </div>
         </div>
