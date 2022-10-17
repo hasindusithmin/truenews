a8244688fff15fdee0f39d32ae846b9b5c6ddf32
hasindu sithmin <hasindusithmin64@gmail.com>
2022-10-17 09:26:37 +0000

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,8 +17,6 @@ export default function Home({handler}) {
             <p><span className="w3-tag">FYI!</span> Are you interested in advertising on our website please contact</p>
             <form target="_blank">
                 <p><input className="w3-input w3-padding-16 w3-border" type="text" placeholder="Name" required="" name="Name" /></p>
-                <p><input className="w3-input w3-padding-16 w3-border" type="number" placeholder="How many people" required="" name="People" /></p>
-                <p><input className="w3-input w3-padding-16 w3-border" type="datetime-local" placeholder="Date and time" required="" name="date" /></p>
                 <p><input className="w3-input w3-padding-16 w3-border" type="text" placeholder="Message \ Special requirements" required="" name="Message" /></p>
                 <p><button className="w3-button w3-black" type="submit">SEND MESSAGE</button></p>
             </form>
